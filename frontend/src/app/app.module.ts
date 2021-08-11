7c2942586aa83630f40bbbfa76c701ac95bbc9b7
MarcosDaNight <marcos.cosson@ccc.ufcg.edu.br>
2021-08-10 21:56:54 -0300

    product.model

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { MatListModule } from "@angular/material/list";
 import { HomeComponent } from "./views/home/home.component";
 import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
 import { RedDirective } from './directives/red.directive';
-import { ProductCreateComponent } from './components/product-create/product-create.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from "@angular/material/button";
 
 
