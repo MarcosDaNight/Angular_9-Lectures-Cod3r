af3da3931c3f2d21554fecbb6a0e7d82bb913d5c
MarcosDaNight <marcos.cosson@ccc.ufcg.edu.br>
2021-08-10 21:52:00 -0300

    product-crud, product-create and navigation button

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { HomeComponent } from "./views/home/home.component";
 import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { ProductCreateComponent } from './components/product-create/product-create.component';
+
 
 const routes: Routes = [
   {
@@ -13,10 +15,14 @@ const routes: Routes = [
     path: "products",
     component: ProductCrudComponent,
   },
+  {
+    path: "products/create",
+    component: ProductCreateComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
